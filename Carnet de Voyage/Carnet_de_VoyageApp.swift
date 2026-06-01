24dae4d9573c9a32ef9744e8d735253254885605
Hayat <hayatzakhoukh@gmail.com>
2026-06-01 14:58:19 +0200

    Initial Commit

---
+++ Carnet de Voyage/Carnet_de_VoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carnet_de_VoyageApp.swift
+//  Carnet de Voyage
+//
+//  Created by apprenant144 on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Carnet_de_VoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carnet de Voyage/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Carnet de Voyage
+//
+//  Created by apprenant144 on 01/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "person")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hayat")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
