090cf2d777fb8fe7a887f68153bb764637b601a5
Hayat <hayatzakhoukh@gmail.com>
2026-06-01 15:37:03 +0200

    Modification taille de texte et alignement au centre

--- Carnet de Voyage/ContentView.swift
+++ Carnet de Voyage/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Carnet de Voyage de Hayat")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
         }
         .padding()
     }
